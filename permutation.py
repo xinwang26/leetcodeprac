7ad88925c0395032e01707a681605fc3911ecd4f
Xin Wang <xinwang26@yahoo.com>
2018-11-29 16:31:00 -0800

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,42 +1,19 @@
-#dictionary method my own 
+#dictionary method my own but slow as 56ms and slower than 48.24% python3 solution
 class Solution:
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        if len(nums) <=1:
+            return [nums]
         nums = sorted(nums)
         global result
-        result = [nums]
-        def appendnextpermute(currpermut,result):
-            # print('bf',currpermut,result)
-            for i in reversed(range(len(nums))):
-                if i-1>=0 and currpermut[i-1] < currpermut[i]:
-                    for j in reversed(range(len(nums))):
-                        if currpermut[j] > currpermut[i-1]:
-                            currpermut[i-1],currpermut[j] = currpermut[j],currpermut[i-1]
-                            currpermut[i:] = sorted(currpermut[i:])
-                            # result = result +[currpermut]
-                            print('af',currpermut,result)
-                            result = result[:] + [appendnextpermute(currpermut[:],result[:])]        
-                            return currpermut
-        appendnextpermute(nums,[nums])
-        return result
-
-#still wrong but better:
-class Solution:
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums = sorted(nums)
-        global result
-        result = [nums]
+        result = [nums[:]]
         def appendnextpermute(currpermut):
-            # print('bf',currpermut,result)
             global result
-            # print(currpermut,result)
+            print(currpermut)
+            # print('bf',currpermut,result)
             for i in reversed(range(len(nums))):
                 if i-1>=0 and currpermut[i-1] < currpermut[i]:
                     for j in reversed(range(len(nums))):
@@ -44,11 +21,9 @@ class Solution:
                             currpermut[i-1],currpermut[j] = currpermut[j],currpermut[i-1]
                             currpermut[i:] = sorted(currpermut[i:])
                             # result = result +[currpermut]
-                            print('bf',currpermut,'__',result)
-                            temp = appendnextpermute(currpermut[:])
-                            if temp != None:
-                                result = [currpermut] + temp
-                            # print('af',currpermut,result)
-                            return result
+                            result.append(appendnextpermute(currpermut[:]))
+                            return currpermut
         appendnextpermute(nums)
+        result =  list(filter(None,result) ) + [nums] 
+        # result = result.remove(null) + [nums]
         return result
